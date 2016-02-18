f38bc2cef88806b03df6e8864948e3dc6a860a24
Kevin Windisch <kwindisch1992@gmail.com>
2016-02-17 22:11:18 -0600

    Added find_by_title and in_stock?

--- lib/product.rb
+++ lib/product.rb
@@ -1,13 +1,38 @@
 class Product
+	attr_accessor :title, :price, :stock
+
 	@@products = []
 
 	def initialize(options={})
-		@@products << self
+		@title = options[:title]
+		@price = options[:price]
+		@stock = options[:stock]
+		add_to_products
 	end
 
 	def self.all
 		@@products
 	end
 
+	def self.find_by_title(title)
+		all.each do |product|
+			if product.title == title then
+				return product
+			else #raise NoProductError,
+				"#{title} does not exist"
+			end
+		end
+	end
+
+	def in_stock?
+		@stock > 0
+	end
+
+	private
+
+	def add_to_products
+		#if :title == title in @@products, raise DuplicateProductError
+		@@products << self
+	end
 
 end
