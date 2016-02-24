7b6ff8f29ed0aee7cad850a8ae9d540a8c22225a
Kevin Windisch <kwindisch1992@gmail.com>
2016-02-23 20:19:00 -0600

    Fixed in_stock

--- lib/product.rb
+++ lib/product.rb
@@ -2,6 +2,7 @@ class Product
 	attr_accessor :title, :price, :stock
 
 	@@products = []
+	@@in_stock = []
 
 	def self.all
 		@@products
@@ -11,7 +12,8 @@ class Product
 		@title = options[:title]
 		@price = options[:price]
 		@stock = options[:stock]
-		add_to_products 
+		add_to_products
+		add_to_in_stock if @stock > 0
 	end
 
 	def self.find_by_title(title)
@@ -29,13 +31,7 @@ class Product
 	end
 
 	def self.in_stock
-		@in_stock = Array.new
-
-		all.each do |product|
-			if product.in_stock? then
-				@in_stock.push(product)
-			end
-		end
+		@@in_stock
 	end
 
 	#### DOESN'T WORK AND I DON'T KNOW WHY. FIGURE IT OUT ####
@@ -48,6 +44,10 @@ class Product
 	def add_to_products
 		#if @title == title in @@products, raise DuplicateProductError
 		@@products << self
+	end
+
+	def add_to_in_stock
+		@@in_stock << self
 	end
 
 end
