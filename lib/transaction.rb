1c600bb7940d392378b65362a296cb6006388201
Kevin Windisch <kwindisch1992@gmail.com>
2016-04-06 15:31:56 -0500

    formatting transaction.rb

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,40 +1,40 @@
 class Transaction
-	attr_reader :id, :product, :customer
+attr_reader :id, :product, :customer
 
-	@@id_generator = 0
-	@@transactions = []
+@@id_generator = 0
+@@transactions = []
 
-	def self.all
-		@@transactions
-	end
+  def self.all
+    @@transactions
+  end
 
-	def initialize(customer, product)
-		if product.in_stock? then
-			@customer = customer
-			@product = product
-			@id = @@id_generator += 1
-			product.stock -= 1
-			add_to_transactions
-		else 
-			raise OutOfStockError, "#{product.title} is out of stock."
-		end
-	end
+  def initialize(customer, product)
+    if product.in_stock? then
+	@customer = customer
+	@product = product
+	@id = @@id_generator += 1
+	product.stock -= 1
+	add_to_transactions
+      else 
+	raise OutOfStockError, "#{product.title} is out of stock."
+      end
+    end
 
-	def self.find(id)
-		all.map do |transaction|
-			if transaction.id == id then
-				return transaction
-			end
-		end
-	end
+  def self.find(id)
+    all.map do |transaction|
+      if transaction.id == id then
+        return transaction
+      end
+    end
+  end
 
-	def self.return(id)
-		transaction = find(id)
-		all.delete(transaction)
-		transaction.product.stock += 1
-	end
+  def self.return(id)
+    transaction = find(id)
+    all.delete(transaction)
+    transaction.product.stock += 1
+  end
 
-	def self.purchases_by(customer)
+  def self.purchases_by(customer)
 		all.each do |transaction|
 			if transaction.customer == customer 
 				puts "Transaction ID: #{transaction.id}    Title: #{transaction.product.title}\n"
